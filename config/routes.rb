e90c4ba5b1cbae4164ae8199675369ee4920f03a
Djorge Gimenez <jrg.gmnz@gmail.com>
2013-03-05 13:36:09 -0300

    Proyecto en Rails (Beta)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Proyect::Application.routes.draw do
   resources :works
   match ':id' => 'works#show', :as => :short_work
 
-
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
