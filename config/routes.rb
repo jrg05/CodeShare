19315a036659b8a3dcfbb314228abd86b7e19bd5
Jorge Gimenez <jrg.gmnz@gmail.com>
2013-03-11 11:32:51 -0300

    Version de prueba con widget de Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,9 @@ Proyect::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
- root :to => 'Home#index'
+
+  match 'static/:action' => 'static#:action'
+  root :to => 'Home#index'
 
   # See how all your routes lay out with "rake routes"
 
